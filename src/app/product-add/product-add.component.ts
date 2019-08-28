fcae53438c2db0a20341f8d5624088a0a003876d
Daniel Moscarella <daniel.moscarella@bbva.com>
2019-08-27 22:35:49 -0500

    initial import

---
+++ src/app/product-add/product-add.component.ts
@@ -0,0 +1,36 @@
+// product-add.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.css']
+})
+export class ProductAddComponent implements OnInit {
+
+  angForm: FormGroup;
+  constructor(private route: ActivatedRoute, private router: Router, private ps: ProductsService, private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.angForm = this.fb.group({
+      ProductName: ['', Validators.required ],
+      ProductDescription: ['', Validators.required ],
+      ProductPrice: ['', Validators.required ]
+    });
+  }
+
+  addProduct(ProductName, ProductDescription, ProductPrice) {
+    this.ps.addProduct(ProductName, ProductDescription, ProductPrice);
+	this.router.navigate(['products']);
+  }
+
+  ngOnInit() {
+  }
+
+}
