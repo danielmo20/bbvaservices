d68f65288938aeb8147c0fe27ed58c13f3303537
Manuel Montanez <mhanuel@gmail.com>
2019-08-28 15:45:14 -0500

    agregar rutas cache

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
 workbox.precaching.precacheAndRoute(
-
   [
       {
         "url":"assets/bbva_logo.png",
@@ -18,5 +17,12 @@ workbox.precaching.precacheAndRoute(
         "url":"index.html",
         "revision":"0a3dd32848cc5474474010fdb1eb72f8"
       },
-    ]
+      { "url":"runtime.js" ,"revision":"0a3dd32848cc5474474010fdb1eb72f8" },
+      { "url":"polyfills.js" ,"revision":"0a3dd32848cc5474474010fdb1eb72f8" },
+      { "url":"styles.js" ,"revision":"0a3dd32848cc5474474010fdb1eb72f8" },
+      { "url":"vendor.js" ,"revision":"0a3dd32848cc5474474010fdb1eb72f8" },
+      { "url":"main.js" ,"revision":"0a3dd32848cc5474474010fdb1eb72f8"},
+      { "url":"assets/mastercard.png" ,"revision":"0a3dd32848cc5474474010fdb1eb72f8"}
+
+      ]
   );
