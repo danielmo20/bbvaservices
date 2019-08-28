274f6bc70fff91f469d82721a32e29013439d79a
mmontanez <mmontanez@koghi.com>
2019-08-28 15:28:59 -0500

    Se agrega modulo con pagina  bbva

---
+++ src/app/product-bbva/product-bbva.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-bbva',
+  templateUrl: './product-bbva.component.html',
+  styleUrls: ['./product-bbva.component.css']
+})
+export class ProductBbvaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
